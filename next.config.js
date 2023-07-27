42eb43cad9bee50eb2dcba8e0a607d77f31cb222
hebbs <hebbsnator@gmail.com>
2023-07-27 22:21:08 +0300

    add basepath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const base_path = process.env.NEXT_PUBLIC_BASE_PATH || ""
+let assetPrefix = base_path + '/'
+let basePath = base_path
+
 const nextConfig = {
-    output:"export"
+    output:"export",
+    assetPrefix: assetPrefix,
+    basePath: basePath,
 }
 
 module.exports = nextConfig
