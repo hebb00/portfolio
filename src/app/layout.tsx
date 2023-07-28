526db43a3e4f85cd5d8408f6be33d690eb6438a7
hebbs <hebbsnator@gmail.com>
2023-07-28 14:58:58 +0300

    fix: remove unused font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import { Navbar } from '../../components/Navbar'
 import './globals.css'
-import { Inter } from 'next/font/google'
 import Footer from '../../components/Footer'
 import Head from 'next/head'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Portfolio',
@@ -18,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
       <Head>
       <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
