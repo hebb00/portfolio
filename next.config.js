7e48bde0f80ee940cb7bc21da551aa764fa9abb0
hebbs <hebbsnator@gmail.com>
2023-07-28 13:56:01 +0300

    test basepath

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
-const base_path = process.env.NEXT_PUBLIC_BASE_PATH || ""
-let assetPrefix = base_path + '/'
-let basePath = base_path
-
+// const base_path = process.env.NEXT_PUBLIC_BASE_PATH || ""
+// let assetPrefix = base_path + '/'
+// let basePath = base_path
+let assetPrefix = '/portfolio/'
+let basePath = '/portfolio'
 const nextConfig = {
     output:"export",
     assetPrefix: assetPrefix,
     basePath: basePath,
 }
-
 module.exports = nextConfig
